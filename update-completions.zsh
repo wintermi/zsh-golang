7c175aaa5c566a88ce3b97d44a7ee61e3f90a5f6
Matthew Winter <33818+wintermi@users.noreply.github.com>
2023-01-27 13:10:12 +1100

    feat: display progress bar when downloading

--- update-completions.zsh
+++ update-completions.zsh
@@ -2,11 +2,11 @@
 # shellcheck disable=SC1090
 
 # Completions directory for `go` command
-local ZSH_GOLANG_DIR="${0:A:h}/completions"
+local DOWNLOAD_DIR="${0:A:h}/completions"
 
 # Download the latest versions of the Go completions
 # Maintained by Koichi Shiraishi - https://github.com/zchee/zsh-completions
 echo "Downloading '_go' ..."
-curl -sSL -o "$ZSH_GOLANG_DIR/_go" "https://raw.githubusercontent.com/zchee/zsh-completions/main/src/go/_go"
+curl -SL --progress-bar -o "$DOWNLOAD_DIR/_go" "https://raw.githubusercontent.com/zchee/zsh-completions/main/src/go/_go"
 echo "Downloading '_gofmt' ..."
-curl -sSL -o "$ZSH_GOLANG_DIR/_gofmt" "https://raw.githubusercontent.com/zchee/zsh-completions/main/src/go/_gofmt"
+curl -SL --progress-bar -o "$DOWNLOAD_DIR/_gofmt" "https://raw.githubusercontent.com/zchee/zsh-completions/main/src/go/_gofmt"
