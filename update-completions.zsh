550545c72d093f76e2d7622f671c096d473cb8b3
Matthew Winter <33818+wintermi@users.noreply.github.com>
2023-08-21 00:53:06 +1000

    feat: update completions for go v1.21

--- update-completions.zsh
+++ update-completions.zsh
@@ -7,6 +7,6 @@ local DOWNLOAD_DIR="${0:A:h}/completions"
 # Download the latest versions of the Go completions
 # Maintained by Koichi Shiraishi - https://github.com/zchee/zsh-completions
 echo "Downloading '_go' ..."
-curl -SL --progress-bar -o "$DOWNLOAD_DIR/_go" "https://raw.githubusercontent.com/zchee/zsh-completions/main/src/go/_go"
+curl -SL --progress-bar -o "$DOWNLOAD_DIR/_go" "https://raw.githubusercontent.com/zsh-users/zsh-completions/master/src/_golang"
 echo "Downloading '_gofmt' ..."
 curl -SL --progress-bar -o "$DOWNLOAD_DIR/_gofmt" "https://raw.githubusercontent.com/zchee/zsh-completions/main/src/go/_gofmt"
